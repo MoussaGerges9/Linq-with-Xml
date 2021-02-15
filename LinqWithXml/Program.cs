beb2d344d0e83b5b7fdaf30cf382999d2fc9e7db
moussagerges <moussagerges@users.noreply.github.com>
2021-02-15 10:24:59 +0200

    Add XML file

--- LinqWithXml/Program.cs
+++ LinqWithXml/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,29 +13,8 @@ namespace LinqWithXml
     {
         static void Main(string[] args)
         {
-            string studentXml = @"
-                             <Students>
-                              <Student>
-                                <Name>Marco</Name>
-                                <Age>19</Age>
-                                <University>Don Bosco</University>
-                              </Student>
-                               <Student>
-                                <Name>Maickel</Name>
-                                <Age>18</Age>
-                                <University>Cairo University</University>
-                              </Student>
-                              <Student>
-                                <Name>Abdo</Name>
-                                <Age>17</Age>
-                                <University>Modern Academy</University>
-                              </Student>
-                              <Student>
-                                <Name>Mina</Name>
-                                <Age>20</Age>
-                                <University>Guc</University>
-                              </Student>
-                             </Students>";
+
+            string studentXml = File.ReadAllText("Xml.txt");
 
             XDocument studentsXdoc = new XDocument();
             studentsXdoc = XDocument.Parse(studentXml);
@@ -65,5 +45,6 @@ namespace LinqWithXml
 
             Console.ReadKey();
         }
+
     }
 }
